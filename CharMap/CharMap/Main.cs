1dcd07757a692f53e8e54561fdd9e696e11445e1
hjgode@gmail.com <hjgode@gmail.com@cc4bc78e-63a6-9522-0a18-abdddc0fe8e2>
2011-04-02 07:59:40 +0000

    added combobox etc

--- CharMap/CharMap/Main.cs
+++ CharMap/CharMap/Main.cs
@@ -32,16 +32,37 @@ namespace CharMap
 			this.SuspendLayout();
 			Button btnOK = new Button ();
 			btnOK.Text = "OK";
-			btnOK.Location = new System.Drawing.Point (this.Height-24, this.Width-80);
+			btnOK.Location = new System.Drawing.Point (4, 4);
 			btnOK.Size = new System.Drawing.Size (80, 24);
 			this.Controls.Add (btnOK);
 			btnOK.Click += new EventHandler (btnOK_Click);
 			
+			lb = new ComboBox();
+			lb.DropDownStyle=ComboBoxStyle.DropDownList;
+			lb.Items.Clear();
+			for (int i=0; i<0xff; i++){
+				lb.Items.Insert(i, i.ToString("x02"));	
+			}
+			lb.SelectedIndex=0x04;
+			lb.Location = new Point(100, 4);
+			lb.Size = new System.Drawing.Size (80, 60);
+			lb.SelectedIndexChanged+=new EventHandler(lb_SelectedIndexChanged);
+			this.Controls.Add(lb);
+				
 			this.Paint+=new PaintEventHandler(Form_Paint);
 			
 			this.ResumeLayout();
 		}
-
+		
+		ComboBox lb;
+		byte bCurrCodepage=0x04;
+		int iOffsetTop = 30;
+		
+		private void lb_SelectedIndexChanged(object sender, EventArgs e){
+			bCurrCodepage=(byte)lb.SelectedIndex;
+			this.Refresh();
+		}
+		
 		private void Form_Paint (object sender, PaintEventArgs e)
 		{
 			/*
@@ -60,11 +81,27 @@ namespace CharMap
 		    */
 			int iX=0, iY=0;
 			int iXstep=30, iYstep=30;
-			byte bHigh=0x04, bLow=0x00;
+			byte bHigh=bCurrCodepage;// 0x04;
+			byte bLow=0x00;
+			
+			
+			Font drawFont = new Font("Arial Unicode MS", 10);
+		    SolidBrush drawBrush = new SolidBrush(Color.Black);
+			for(iX=0; iX<16; iX++){
+				e.Graphics.DrawString(iX.ToString("x2"), 
+				        drawFont, drawBrush, 
+				        new Point(iX*iXstep+iXstep, iYstep));
+			}			
+			for(iY=0; iY<16; iY++){
+				int xx = (iY*0x10);
+				e.Graphics.DrawString(xx.ToString("x2"), 
+				        drawFont, drawBrush, 
+				        new Point(0, iY* iYstep + iOffsetTop+iYstep));
+			}
 			
 			for(iY=0; iY<16; iY++){
 				for (iX=0; iX<16; iX++){
-					Point p = new Point(iX*iXstep, iY*iYstep);
+					Point p = new Point(iX*iXstep+iXstep, iY*iYstep + iOffsetTop + iYstep);
 					byte[] b=new byte[2];
 					b[0]=bLow++; b[1]=bHigh;
 					
